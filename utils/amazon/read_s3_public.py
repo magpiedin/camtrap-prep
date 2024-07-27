4a44a41eef8265f97e23c6315309a64972918cd5
magpiedin <magpiedin@gmail.com>
2024-07-27 11:25:36 -0500

    let filter call read_s3 functions

--- utils/amazon/read_s3_public.py
+++ utils/amazon/read_s3_public.py
@@ -1,25 +1,31 @@
 # Parse Public File to Extract All
+import sys
 import requests
 import pandas as pd
 import datetime, time
+from dotenv import dotenv_values
 from xml.etree import ElementTree
 
-bucket_name = 'urbanriverrangers'
-base_url = f'https://{bucket_name}.s3.amazonaws.com'
-
     
-def list_objects(bucket_url, continuation_token=None):
-    params = {'list-type': '2'}  # Use list-type=2 for continuation token support
+def list_objects(bucket_url, filter, continuation_token=None):
+    params = {'list-type':'2'} # Use list-type=2 for continuation token support
+    # params['prefix'] = filter
     if continuation_token:
         params['continuation-token'] = continuation_token
 
-    response = requests.get(bucket_url, params=params)
+    full_url = f'{bucket_url}{filter}'
+
+    print(f'full url = {full_url}')
+
+    response = requests.get(full_url, params=params)
     response.raise_for_status()  # Ensure we get a successful response
 
     return response.text
 
+
 def parse_objects(xml_response):
     root = ElementTree.fromstring(xml_response)
+
     # Define the namespace
     namespace = {'ns': 'http://s3.amazonaws.com/doc/2006-03-01/'}
     data = []
@@ -34,40 +40,73 @@ def parse_objects(xml_response):
     next_token = root.find('.//{http://s3.amazonaws.com/doc/2006-03-01/}NextContinuationToken')
     return data, next_token.text if next_token is not None else None
 
-continuation_token = None
-all_data = []
 
-i = 0
-while True:
-    xml_response = list_objects(base_url, continuation_token)
-    data, continuation_token = parse_objects(xml_response)
-    all_data.extend(data)
+def get_s3_keys():
+    '''main function to retrieve keys from sn s3 bucket, given a filter'''
 
-    time.sleep(2)
+    config = dotenv_values()
 
-    i += 1
-    if len(data) > 0:
-        sample_row = data[0]
-    else: data = ''
-    print(f'~ {datetime.datetime.now()} - Getting XML ~ page {i} ~ sample row: {sample_row}')
+    bucket_name = 'urbanriverrangers'
+    base_url = f'https://{bucket_name}.s3.amazonaws.com'
 
-    if i == 10:
-        continuation_token = None
+    # Get filters
+    filter = ''
+ 
+    # # 1 - check cli args
+    if len(sys.argv) > 1:
+        filter = f'/?prefix=images/{sys.argv[1][:4]}/{sys.argv[1]}'  # ex '/?prefix=images/2024/2024-07-20_UR004'
     
-    if continuation_token is None:
-        break
+    # # 2 - check .env
+    elif config['INPUT_S3_FILTER'] is not None:
+        filter = config['INPUT_S3_FILTER']
 
-# Store keys in a pandas DataFrame
-df = pd.DataFrame(all_data, columns=['Key','ETag','LastModified'])
+    continuation_token = None
+    all_data = []
 
-# Save the DataFrame to a CSV file
-print(f'~ {datetime.datetime.now()} - Saving df to CSV...')
-csv_file = 's3_keys.csv'
-df.to_csv(csv_file, index=False)
+    i = 0
+    while True:
+        xml_response = list_objects(base_url, filter, continuation_token)
+        data, continuation_token = parse_objects(xml_response)
+        all_data.extend(data)
 
-print(f"~ {datetime.datetime.now()} - Keys have been saved to {csv_file}")
+        time.sleep(2)
 
-# Step 2: Parse the XML generated in s3_keys (also df) into a media table?
-# Alternately, we could do this right after filtering
+        i += 1
+        sample_row = ''
+        if len(data) > 0:
+            sample_row = data[0]
+        else: data = ''
+        print(f'~ {datetime.datetime.now()} - Getting XML ~ page {i} ~ sample row: {sample_row}')
 
-# Step 3: Grab Exif Information
+        if i == 10:
+            continuation_token = None
+        
+        if continuation_token is None:
+            break
+
+    # Store keys in a pandas DataFrame
+    df = pd.DataFrame(all_data, columns=['Key','ETag','LastModified'])
+
+
+    # Step 2: Parse the XML generated in s3_keys (also df) into a media table?
+    # # Alternately, we could do this right after filtering
+
+
+    # Step 3: Grab Exif Information
+
+
+    # OPTIONALLY Save the DataFrame to a CSV file
+    to_csv = sys.argv[2]
+    if to_csv == "CSV":
+        print(f'~ {datetime.datetime.now()} - Saving df to CSV...')
+        csv_file = 's3_keys.csv'
+        df.to_csv(csv_file, index=False)
+        
+        print(f"~ {datetime.datetime.now()} - Keys have been saved to {csv_file}")
+    
+    else:
+        return df
+
+
+if __name__ == "__main__":
+    get_s3_keys()
